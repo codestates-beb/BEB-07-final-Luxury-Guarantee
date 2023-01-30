81325f060be0634066574fcb0c5a686f7a95ecbd
baekhun2463 <baekhun2463@gmail.com>
2023-01-30 22:02:15 +0900

    회원가입 기능 70%

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
 import {
     LOGIN_USER,
+    SIGNUP_USER
 } from './types';
 
 export function loginUser(dataToSubmit) {
 
-    const request = axios.post('http://43.206.226.112:8080/signin', dataToSubmit)
+    const request = axios.post('/signin', dataToSubmit)
         .then(response => response.data)
 
     return {
         type: LOGIN_USER,
+        payload: request
+    }
+}
+
+
+export function signupUser(dataToSubmit) {
+
+    const request = axios.post('/newuser', dataToSubmit)
+        .then(response => response.data)
+
+    return {
+        type: SIGNUP_USER,
         payload: request
     }
 }
--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,6 +1,9 @@
 import { 
-    LOGIN_USER
+    LOGIN_USER,
+    SIGNUP_USER
 } from '../_actions/types'
+
+// eslint-disable-next-line import/no-anonymous-default-export
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = {}, action) {
@@ -8,6 +11,9 @@ export default function (state = {}, action) {
         case LOGIN_USER:
             return { ...state, loginSuccess: action.payload }
             
+        case SIGNUP_USER:
+            return { ...state, register: action.payload}
+           
         default:
             return state;
     }
