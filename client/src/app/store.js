3a2a3ff7665347735d8aece8f18a4f1e8b3ea62a
fromdwlee <desgod@naver.com>
2023-02-01 16:22:58 +0900

    redux delete

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,13 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import { atom } from 'recoil';
-
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
 
 export const status = atom({
   key: 'signStatus',
