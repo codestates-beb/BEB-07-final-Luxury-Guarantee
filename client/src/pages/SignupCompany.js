930299382a989f4b181684b327d18572d6c028db
baekhun2463 <baekhun2463@gmail.com>
2023-02-03 18:24:43 +0900

    회원가입 수정 및 기업용 회원가입 페이지에 모달추가

--- client/src/pages/SignupCompany.js
+++ client/src/pages/SignupCompany.js
@@ -1,97 +1,150 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { useRecoilState } from 'recoil';
 import apiUrl from '../utils/api';
+import CompanyModal from '../components/CompanyModal';
 //기업용 회원가입 페이지
 
 //모달창으로 법적고지 설명하기 UI작업 종료후 기능구현때 구현하기.
 const SignupCompany = () => {
 
-    const [Email, setEmail] = useState("");
-    const [Name, setName] = useState("");
-    const [Password, setPassword] = useState("");
-    const [ConfirmPassword, setConfirmPassword] = useState("");
-   
-    const onEmailHandler = (event) => {
-        setEmail(event.currentTarget.value);
-    }
-    const onNameHandler = (event) => {
-        setName(event.currentTarget.value);
-    }
-    const onPasswordHandler = (event) => {
-        setPassword(event.currentTarget.value);
-    }
-    const onConfirmPasswordHandler = (event) => {
-        setConfirmPassword(event.currentTarget.value);
-    }
-    const onSubmitHandler = () => {
-        if(Password !== ConfirmPassword){
-            return alert("비밀번호가 다릅니다.")
-        }
+    const [Id, setId] = useState("");
+    const [IdVaild, setIdValid] = useState("");
+    const [isId, setIsId] = useState(false);
 
+    const [Name, setName] = useState("");
+    const [NameValid, setNameValid] = useState("");
+    const [isName, setIsName] = useState(false);
+
+    const [Password, setPassword] = useState("");
+    const [PasswordValid, setPasswordValid] = useState("");
+    const [isPassword, setIsPassword] = useState(false)
+
+    const [AlertMessage, setAlertMessage] = useState("")
+
+    const onCheckPassword = (event) => {
+        //  6 ~ 18자 영문, 숫자 조합
+        const currentPassword = event.currentTarget.value;
+        setPassword(currentPassword);
+
+        const regExp = /^[a-zA-z0-9]{6,18}$/
+        
+        if(!regExp.test(currentPassword)) {
+            setPasswordValid("숫자+영문자 6자리 이상 입력해주세요")
+            setIsPassword(false)
+        } else {
+            setPasswordValid("안전한 비밀번호입니다.")
+            setIsPassword(true)
+        }
+    }
+
+    const onCheckId = (event) => {
+        const currentId = event.currentTarget.value;
+        setId(currentId);
+
+        if(currentId.length < 4) {
+            setIdValid("이메일 형식에 맞게 작성해주세요")
+            setIsId(false);
+        } else {
+            setIdValid("")
+            setIsId(true);
+        }
+    }
+
+    const onCheckName = (event) => {
+        const currentName = event.currentTarget.value;
+        setName(currentName)
+        if(currentName.length < 3) {
+            setNameValid("3자리 이상 입력해주세요.")
+            setIsName(false);
+        }else {
+            setNameValid("")
+            setIsName(true);
+        }
+    }
+
+    const onSubmitHandler = () => {
         axios.post(`${apiUrl}/newcompany`,{
-            userId: Email,
+            userId: Id,
             nickname: Name,
             password: Password
             
         })
             .then(res => {
-              const signData={userId: res.data.message.userId,
-                address: res.data.message.address,
-                nickname: res.data.message.nickname,
-                isCompany: res.data.message.isCompany,
-                isSigned: true} 
-              console.log (signData)
-               sessionStorage.setItem('signdata', JSON.stringify(signData))
+                if(res.data.message === "same id exist"){
+                    setAlertMessage("아이디가 중복됩니다.")
+                } 
+                else if(res.data.message === "same nickname exist"){
+                    setAlertMessage("닉네임이 중복됩니다.")
+                }
+                else {
+                
+                console.log(res.data)
+                const signData = {
+                    userId: res.data.message.userId,
+                    address: res.data.message.address,
+                    nickname: res.data.message.nickname,
+                    isCompany: res.data.message.isCompany,
+                    isSigned: true
+                }
+                sessionStorage.setItem('signData', JSON.stringify(signData))
                 document.location.href = '/'
-            })
+    }})
+        
     }
+
 
     return (
         <div className='flex justify-center mb-10 mt-10'>
 
-<div className="flex flex-col max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
-    <div className="self-center mb-2 text-xl font-light text-gray-800 sm:text-2xl dark:text-white">
-        Create a new account
-    </div>
-    <span className="justify-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400">
-        Already have an account ?
-        <Link to="/login" className="text-sm ml-1 text-blue-500 underline hover:text-blue-700">
-            Sign in
-            </Link>
-    </span>
-    <div className="p-6 mt-2">
-        <form action="#">
-            <div className="flex flex-col mb-2">
-                <div className=" relative ">
-                    <input type="text" value={Name} onChange={onNameHandler} id="create-account-companyname" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" name="CompanyName" placeholder="Company Name"/>
-                    </div>
-                    <div className=" relative mt-2">
-                        <input type="number" value={Name} onChange={onPasswordHandler} id="create-account-companynumber" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" name="CompanyNumber" placeholder="Company Number"/>
-                    </div>
+            <div className="flex flex-col max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
+                <div className="self-center mb-2 text-xl font-light text-gray-800 sm:text-2xl dark:text-white">
+                    Create a new account
                 </div>
-                <div className="flex gap-4 mb-2">
-                    <div className=" relative ">
-                        <input type="email" value={Email} onChange={onEmailHandler} id="create-account-email" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="Email"/>
+                <span className="justify-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400">
+                    Already have an account ?
+                    <Link to="/login" className="text-sm ml-1 text-blue-500 underline hover:text-blue-700">
+                        Sign in
+                    </Link>
+                </span>
+                <div className="p-6 mt-2">
+                        <div className="flex flex-col mb-3">
+                            <div className=" relative">
+                                <input type="text" value={Name} onChange={onCheckName} id="create-account-username" className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" name="username" placeholder="NickName" />
+                                <p className='text-white text-base mt-2'>{NameValid}</p>
+                            </div>
+
                         </div>
-                       
-                        </div>
-                        <div className="flex flex-col mb-2">
+                        <div className="flex flex-col mb-3">
                             <div className=" relative ">
-                                <input type="password" value={Password} onChange={onPasswordHandler} id="create-account-password" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="PassWord"/>
-                                </div>
+                                <input type="email" value={Id} onChange={onCheckId} id="create-account-email" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="Id" />
+                                <p className='text-white text-base mt-2'>{IdVaild}</p>
                             </div>
-                            <div className="flex w-full my-4">
-                                <button type="submit" className="py-2 px-4  bg-black hover:bg-black focus:ring-black focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                                    Login
-                                </button>
+
+                        </div>
+                        <div className="flex flex-col mb-3">
+                            <div className=" relative ">
+                                <input type="password" value={Password} onChange={onCheckPassword} id="create-account-password" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="PassWord" />
                             </div>
-                        </form>
-                        
-                    </div>
+                        </div>
+                        <div className="flex flex-col">
+                            <div className=" relative ">
+                                <input type="password" id="create-account-password" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent" placeholder="ConfirmPassWord" />
+                                <p className='text-white text-xs mt-2'>{PasswordValid}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <CompanyModal/>
+                        </div>
+                        <div className="flex w-full mt-4">
+                            <button onClick={onSubmitHandler} className="py-2 px-4  bg-black hover:bg-black focus:ring-black focus:ring-offset-black text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                Login
+                            </button>
+                        </div>
+
                 </div>
             </div>
+        </div>
     )
 }
 
