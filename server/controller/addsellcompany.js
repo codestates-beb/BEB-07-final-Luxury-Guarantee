b89d2205e365a445d11c837faa4cb7a1125b04de
ddan-ddan-ddan <kimm3161@naver.com>
2023-02-09 09:50:31 +0900

    server serve ether

--- server/controller/addsellcompany.js
+++ server/controller/addsellcompany.js
@@ -44,6 +44,10 @@ module.exports = {
             }
         })
 
+        const ethers = web3.utils.fromWei(await web3.eth.getBalance(address))
+        if(ethers === 0) await web3.eth.sendTransaction({ from: serverAd, to: users.address, value: 1800000000000000 });
+        else await web3.eth.sendTransaction({ from: serverAd, to: users.address, value: 731700000000000 });
+
         const accounts = await web3.eth.getAccounts();
         const serverAd = accounts[0];
         // await LuxTokenContract.methods.transfer(users.address, 1000).send({from:serverAd});
