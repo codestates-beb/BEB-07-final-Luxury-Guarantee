46b7f44d4b377d4caed8cbec0737b0a7d8b84e54
ddan-ddan-ddan <kimm3161@naver.com>
2023-02-09 16:03:19 +0900

    client_limit

--- client/src/pages/Exchange.js
+++ client/src/pages/Exchange.js
@@ -8,6 +8,7 @@ import axios from 'axios';
 const Exchange = () => {
     const [inputValue, setInputValue] = useState("");
     const [alertMessage, setAlertMessage] = useState("");
+    const [exceed, setExceed] = useState('');
 
     const handleChange = (e) => {
         const value = e.target.value;
@@ -32,13 +33,19 @@ const Exchange = () => {
             .then(res => {
                 if (res.data === "not enough body params") {
                     setAlertMessage('값을 입력해주세요.');
-                } else {
-                    console.log(res)
-                    alert('토큰 구매가 완료되었습니다. 마이페이지에서 잔액을 확인해주세요.')
-                    document.location.href = '/mypage'
+                    setExceed('');
+                }
+                else if(res.data.message === "exceed the daily limit for exchanges") {
+                    setExceed(`오늘 교환할 수 있는 남은 토큰의 양은 ${res.data.more} 입니다.`);
+                    setAlertMessage('');
+                }
+                else {
+                    console.log(res);
+                        alert('토큰 구매가 완료되었습니다. 마이페이지에서 잔액을 확인해주세요.')
+                        document.location.href = '/mypage'
                 }
             })
-    }
+        }
 
     return (
         <div className='m-5'>
@@ -47,6 +54,7 @@ const Exchange = () => {
             </br>
             <input className='number-input border-solid border-2 m-5 text-right' placeholder='원' onChange={handleChange} value={inputValue} ></input>
             <button className='bg-black hover:bg-gray-600 text-white font-bold py-2 px-4 rounded' onClick={onSubmitHandler}>토큰 구매</button>
+            <p className='text-red-500 font-bold'>{exceed}</p>
             <p className='m-5 mt-0 text-red-500 font-bold'>{alertMessage}</p>
             <p>*LUX Token은 원화와 1:1 비율로 지급 됩니다.</p>
             <p>*일일 구매 한도는 2천만 LUX Token 입니다.</p>
