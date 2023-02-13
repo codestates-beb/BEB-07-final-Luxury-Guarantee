4ad02b00e1a34292ac40a854f472f4577b47ec85
fromdwlee <desgod@naver.com>
2023-02-13 09:30:21 +0900

    헤더에 faq 링크 추가 및 오류 수정

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,66 +1,66 @@
 import React from 'react';
 
 const Footer = () => {
-    return(
-        <div className='h-full w-full bg-black flex items-stretch'>
-        <div className='h-[580px] w-[480px] self-auto'>
-            <div className='text-[32px] text-white p-[20px]'><span>About</span></div>
-             <div className='text-[16px] text-white p-[20px]'>We,the Luxury Guarantee, are a non-profit association of luxury brands investing in technologies 
-to enhance the customer experience and build a virtuous future for luxury.
-We strive to make blockchain solutions and related technologies easy and available to all luxury brands.
-We strive to provide tools to assist in creating transparency and trust for all customers and stakeholders.</div>
-        </div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <div className='h-[580px] w-[480px] self-auto'>
-        <div className='text-[32px] text-white  p-[20px]'>Solutions</div>
-        <div className='text-[16px] text-white p-[20px]'>The Platform uses Quorum permissioned blockchain
-technology at its core. Quorum maintains a common
-ledger that can only be updated, block after block, by 
-the network consensus that members provide. Each
-Brand Stack has to manage at least two Quorum nodes and ensure their liveness.
-This blockchain network is completed by the centralized applications part: Blockchain services that allow the connection between the blockchain and other applications.
-Off-chain services including APIs used to connect business services to the Platform.
-Front-end applications for consumer facing interactions.
-API documentation
-The Luxury Guarantee provides a state of the art API to interact with the blockchain network. All complexity is abstracted by Luxury Guarantee and any developer can easily and rapidly handle all interactions sith smart contarcts.</div>
-        </div>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <div className='h-[580px] w-[480px] self-auto'>
-        <div className='text-[32px] text-white p-[20px]'>Membership</div>
-        <div className='text-[16px] text-white p-[20px]'>Luxury Guarantee General
-Luxury Guarantee is a self-managed offering for brands that care about decentralization and want control over data. The Luxury Guarantee’s software is run directly by the brand and the brand participates  in the blockchain governance.
-Luxury Guarantee Corporate
-Luxury Guarantee Corporate is an offering for brands that want to quickly leverage the Luxury Guarantee’s software without the hurdle of hosting the solution.
-Brands use blockchain as a service without running any node.
+        return (
+                <div className='h-full w-full bg-black flex items-stretch '>
+                        <div className='h-[580px] w-[480px] self-auto '>
+                                <div className='text-[32px] text-white p-[20px]'><span>About</span></div>
+                                <div className='text-[16px] text-white p-[20px]'>We,the Luxury Guarantee, are a non-profit association of luxury brands investing in technologies
+                                        to enhance the customer experience and build a virtuous future for luxury.
+                                        We strive to make blockchain solutions and related technologies easy and available to all luxury brands.
+                                        We strive to provide tools to assist in creating transparency and trust for all customers and stakeholders.</div>
+                        </div>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <div className='h-[580px] w-[480px] self-auto'>
+                                <div className='text-[32px] text-white  p-[20px]'>Solutions</div>
+                                <div className='text-[16px] text-white p-[20px]'>The Platform uses Quorum permissioned blockchain
+                                        technology at its core. Quorum maintains a common
+                                        ledger that can only be updated, block after block, by
+                                        the network consensus that members provide. Each
+                                        Brand Stack has to manage at least two Quorum nodes and ensure their liveness.
+                                        This blockchain network is completed by the centralized applications part: Blockchain services that allow the connection between the blockchain and other applications.
+                                        Off-chain services including APIs used to connect business services to the Platform.
+                                        Front-end applications for consumer facing interactions.
+                                        API documentation
+                                        The Luxury Guarantee provides a state of the art API to interact with the blockchain network. All complexity is abstracted by Luxury Guarantee and any developer can easily and rapidly handle all interactions sith smart contarcts.</div>
+                        </div>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <div className='h-[580px] w-[480px] self-auto'>
+                                <div className='text-[32px] text-white p-[20px]'>Membership</div>
+                                <div className='text-[16px] text-white p-[20px]'>Luxury Guarantee General
+                                        Luxury Guarantee is a self-managed offering for brands that care about decentralization and want control over data. The Luxury Guarantee’s software is run directly by the brand and the brand participates  in the blockchain governance.
+                                        Luxury Guarantee Corporate
+                                        Luxury Guarantee Corporate is an offering for brands that want to quickly leverage the Luxury Guarantee’s software without the hurdle of hosting the solution.
+                                        Brands use blockchain as a service without running any node.
 
-</div>
-</div>
-        
-<br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <div className='h-[580px] w-[480px] self-auto'>
-        <div className='text-[32px] text-white p-[20px]'>Resources</div>
-        <div className='text-[16px] text-white p-[20px]'>Leveraging Blockchain for Supply Chain Transparency in the Luxury Sector.
-Using Blockchain to Enhance Luxury Supply Chain Traceability.
-Blockchain introduces a shared ledger that can be used as a real-time source of fact between parties in the supply chain. Assets, which could include inventory or credit notes,for instance,are represented on the blockchain to facilitate transactions between parties. RFID tags,NFC chips or QR codes can make the connection to physical assets where needed.
-Luxury brands can realize substantial benefits from joining a dedicated supply chain network like Luxury Guarantee. Thanks to a shared, real-time point of truth,blockchain reduces operational errors like missing stock or wrong inventory data. 
-</div> 
-        </div>
-        </div>
-    );
+                                </div>
+                        </div>
+
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <div className='h-[580px] w-[480px] self-auto'>
+                                <div className='text-[32px] text-white p-[20px]'>Resources</div>
+                                <div className='text-[16px] text-white p-[20px]'>Leveraging Blockchain for Supply Chain Transparency in the Luxury Sector.
+                                        Using Blockchain to Enhance Luxury Supply Chain Traceability.
+                                        Blockchain introduces a shared ledger that can be used as a real-time source of fact between parties in the supply chain. Assets, which could include inventory or credit notes,for instance,are represented on the blockchain to facilitate transactions between parties. RFID tags,NFC chips or QR codes can make the connection to physical assets where needed.
+                                        Luxury brands can realize substantial benefits from joining a dedicated supply chain network like Luxury Guarantee. Thanks to a shared, real-time point of truth,blockchain reduces operational errors like missing stock or wrong inventory data.
+                                </div>
+                        </div>
+                </div>
+        );
 };
 export default Footer;
